f10fa5331697f6f5bd809d02846d704647040eba
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-02-17 19:41:56 +0800

    fix: centering posts

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -7,11 +7,11 @@ const props = defineProps<{ blog: MicroBlog; distance: number }>()
 <template>
   <div
     w-80 m-4 p-4
-    bg-white bg-opacity-80 preserve-3d
-    class="absolute top-1/3 odd:left-0 even:right-0"
+    bg-white bg-opacity-80 preserve-3d absolute
+    class="top-1/2 odd:left-0 even:right-0"
     border="1 blue-500/50 rounded-md"
     :style="{
-      transform: `translateZ(${-props.distance}px)`,
+      transform: `translateY(-50%) translateZ(${-props.distance}px)`,
     }"
     v-html="props.blog.bodyHTML"
   />
