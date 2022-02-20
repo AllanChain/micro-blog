995b0af915fd070d5a363b6374942fd359957f30
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-02-20 12:04:19 +0800

    feat: read more hint

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -2,15 +2,36 @@
 import type { MicroBlog } from '~/types'
 
 const props = defineProps<{ blog: MicroBlog }>()
+
+const divElement = ref<HTMLDivElement>()
+
+const showReadMore = computed(() => {
+  if (!divElement.value)
+    return false
+  return divElement.value.clientHeight < divElement.value.scrollHeight
+})
 </script>
 
 <template>
   <div
-    w-80 max-w-full max-h-90vh p-4
-    overflow-y-auto
-    prose prose-slate dark:prose-invert
+    ref="divElement"
+    relative
+    w-80 max-w-full max-h-70vh p-4
+    overflow-hidden
+    prose="~ slate dark:invert"
     border="1 blue-500/50 rounded-md"
     bg="white/80 dark:gray-800/80"
-    v-html="props.blog.bodyHTML"
-  />
+  >
+    <div v-html="props.blog.bodyHTML" />
+    <div
+      v-if="showReadMore"
+      absolute bottom-0 left-0
+      pb-2 pt-8 w-full text-center
+      bg-gradient="to-b from-green-50/0 via-blue-50/80 to-blue-50/100"
+      dark:bg-gradient="from-gray-800/0 via-blue-gray-900/80 to-warm-gray-900/100"
+      text="bluegray-500 dark:gray-400"
+    >
+      Click to Read More
+    </div>
+  </div>
 </template>
